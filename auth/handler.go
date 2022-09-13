f61df459b100bf78327b3c508cba88c7f000bd5f
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-13 23:24:56 +0700

    define handler for wrap input from client and give response json

---
+++ auth/handler.go
@@ -0,0 +1,110 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"movie-api/helper"
+	"net/http"
+	"path/filepath"
+)
+
+type authHandler struct {
+	authService Service
+}
+
+func NewAuthHandler(authService Service) *authHandler {
+	return &authHandler{authService}
+}
+
+func (handler *authHandler) SignUpHandler(context *gin.Context) {
+	input := SignUpInput{
+		Name:     context.PostForm("name"),
+		Email:    context.PostForm("email"),
+		Password: context.PostForm("password"),
+	}
+
+	err := context.ShouldBind(&input)
+	if err != nil {
+		errors := helper.ErrorValidationResponse(err)
+
+		errorMsg := gin.H{"errors": errors}
+
+		errorResponse := helper.ApiFailedResponse(errorMsg)
+		context.JSON(http.StatusUnprocessableEntity, errorResponse)
+		return
+	}
+
+	imageAvatar, err := context.FormFile("avatar")
+	if imageAvatar == nil {
+		errorResponse := helper.ApiFailedResponse("Sorry, you must upload an avatar image")
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+	if err != nil {
+		errorResponse := helper.ApiFailedResponse(err.Error())
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	extension := filepath.Ext(imageAvatar.Filename)
+
+	if extension != ".jpg" && extension != ".jpeg" && extension != ".png" {
+		errorResponse := helper.ApiFailedResponse("Sorry, only image file type can be uploaded")
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	if imageAvatar.Size > 1572864 {
+		errorResponse := helper.ApiFailedResponse("Sorry, image uploaded is more than limit (1,5 mb)")
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	newFileName := uuid.New().String() + extension
+
+	imagePath := "avatar/" + newFileName
+
+	err = context.SaveUploadedFile(imageAvatar, imagePath)
+	if err != nil {
+		errorResponse := helper.ApiFailedResponse("failed to save uploaded file")
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	input.Avatar = imagePath
+
+	isUserExist, err := handler.authService.CheckUserAvailabilityByEmail(input)
+	if isUserExist {
+		errorResponse := helper.ApiFailedResponse(err.Error())
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	endpoint := context.Request.URL.String()
+
+	if endpoint == "/api/v1/auth/signup-customer" {
+		user, err := handler.authService.SignUp(input, false)
+		if err != nil {
+			errorResponse := helper.ApiFailedResponse(err.Error())
+			context.JSON(http.StatusBadRequest, errorResponse)
+			return
+		}
+
+		responseFormatter := FormatUserSignupResponse(user, imagePath)
+		successResponse := helper.ApiSuccessResponse("Sign up for customer successfully", responseFormatter)
+
+		context.JSON(http.StatusCreated, successResponse)
+	} else {
+		user, err := handler.authService.SignUp(input, true)
+		if err != nil {
+			errorResponse := helper.ApiFailedResponse(err.Error())
+			context.JSON(http.StatusBadRequest, errorResponse)
+			return
+		}
+
+		responseFormatter := FormatUserSignupResponse(user, imagePath)
+		successResponse := helper.ApiSuccessResponse("Sign up for admin successfully", responseFormatter)
+
+		context.JSON(http.StatusCreated, successResponse)
+	}
+}
