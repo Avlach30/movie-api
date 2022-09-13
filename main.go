5e56b009e5fec050b765a9cacdbae07fcfe88405
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-13 23:26:49 +0700

    define router for sign up feature

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"log"
+	"movie-api/auth"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func main() {
-	
+	dsn := "root:rootPassword!@tcp(127.0.0.1:3306)/movie-api-onboarding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Println("Successfully connected to database!")
+
+	userRepository := auth.NewRepository(db)
+	userService := auth.NewService(userRepository)
+	userHandler := auth.NewAuthHandler(userService)
+
+	router := gin.Default()
+	firstVerAPI := router.Group("/api/v1")
+
+	firstVerAPI.POST("/auth/signup-customer", userHandler.SignUpHandler)
+	firstVerAPI.POST("/auth/admin/signup", userHandler.SignUpHandler)
+
+	router.Run()
 }
