80aa8b52889988e5b6fbe93e33fd1656926d02a6
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-13 23:25:34 +0700

    update structure of input for signup

--- auth/input.go
+++ auth/input.go
@@ -3,6 +3,6 @@ package auth
 type SignUpInput struct {
 	Name     string `form:"name" binding:"required"`
 	Email    string `form:"email" binding:"required,email"`
-	Password string `form:"password" binding:"required,min=10,alphanum,uppercase,lowercase"`
-	Avatar   string `form:"avatar" binding:"required"`
+	Password string `form:"password" binding:"required,min=10,alphanum"`
+	Avatar   string 
 }
