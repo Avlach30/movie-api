b90aee90914ed1bcdf3764870b3cd6d70e5cf9ba
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-13 23:23:54 +0700

    define repository for sign up feature with manipulate data through database with orm

---
+++ auth/repository.go
@@ -0,0 +1,37 @@
+package auth
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	SignUp(user User) (User, error)
+	FindByEmail(email string) (User, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (repo *repository) SignUp(user User) (User, error) {
+	err := repo.db.Create(&user).Error
+
+	if (err != nil) {
+		return user, err
+	} else {
+		return user, nil
+	}
+}
+
+func (repo *repository) FindByEmail(email string) (User, error) {
+	var user User
+
+	err := repo.db.Where("email = ?", email).Find(&user).Error
+	if (err != nil) {
+		return user, err
+	} else {
+		return user, nil
+	}
+}
