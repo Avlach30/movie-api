a3f9c9edc5f4f0e72cf6a6eb7ba58edf3953ebfc
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-14 09:55:45 +0700

    format response body for data key when login successfully

--- auth/format.response.go
+++ auth/format.response.go
@@ -1,18 +1,36 @@
 package auth
 
 type SignUpResponse struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	Email      string `json:"email"`
-	Avatar	   string `json:"avatar"`
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Avatar string `json:"avatar"`
+}
+
+type LoginResponse struct {
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Avatar string `json:"avatar"`
+	Token  string `json:"_token"`
 }
 
 func FormatUserSignupResponse(user User, imageLocation string) SignUpResponse {
 	format := SignUpResponse{
-		ID:         user.ID,
-		Name:       user.Name,
-		Email:      user.Email,
-		Avatar:		imageLocation,
+		ID:     user.ID,
+		Name:   user.Name,
+		Email:  user.Email,
+		Avatar: imageLocation,
+	}
+
+	return format
+}
+
+func FormatUserLoginResponse(user User, token string) LoginResponse {
+	format := LoginResponse{
+		Name: user.Name,
+		Email: user.Email,
+		Avatar: user.Avatar,
+		Token: token,
 	}
 
 	return format
