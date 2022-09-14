b467cb52a14ba806f63d35e37b84ad2ef62f05fd
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-14 09:55:07 +0700

    define service for business logic of login feature with generate token

--- auth/service.go
+++ auth/service.go
@@ -2,6 +2,8 @@ package auth
 
 import (
 	"errors"
+
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -9,6 +11,8 @@ type Service interface {
 	SignUp(input SignUpInput, isAdmin bool) (User, error)
 	CheckUserAvailabilityByEmail(input SignUpInput) (bool, error)
 	UploadAvatarImage(fileLocation string) (string, error)
+	LogIn(input LogInInput) (User, string, error)
+	GenerateToken(userId int, email string, isAdmin bool) (string, error)
 }
 
 type service struct {
@@ -65,4 +69,52 @@ func (service *service) SignUp(input SignUpInput, isAdmin bool) (User, error) {
 	}
 
 	return newUser, nil
+}
+
+func (service *service) GenerateToken(userId int, email string, isAdmin bool) (string, error) {
+
+	//* Generate payload token
+	claim := jwt.MapClaims{
+		"userId": userId,
+		"email": email,
+		"isAdmin": isAdmin,
+	}
+
+	//* Generate token with signing method and payload
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	//* Signature token with secret text
+	signedToken, err := token.SignedString([]byte("$2a$08$UcyjEygcPA/XaeUp85sQjuOhithx14/Ai3D5lYPixLrMrSQG2NIFy"))
+	if (err != nil) {
+		return signedToken, err
+	}
+
+	return signedToken, nil
+}
+
+func (service *service) LogIn(input LogInInput) (User, string, error) {
+	email := input.Email
+	password := input.Password
+
+	user, err := service.repository.FindByEmail(email)
+	if err != nil {
+		return user, "", err
+	}
+
+	if user.ID == 0 {
+		return user, "", errors.New("User with that email not found")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	//* If error (compare result is different)
+	if err != nil {
+		return user, "", errors.New("incorrect Password")
+	}
+
+	userToken, err := service.GenerateToken(user.ID, user.Email, user.IsAdmin)
+	if err != nil {
+		return user, "", errors.New("failed to generate token")
+	}
+
+	return user, userToken, nil
 }
