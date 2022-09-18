69c0136a1ed39bb49064593b1875640f54770a8a
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-18 11:48:49 +0700

    calling function of configure gorm connect to main function of go apps

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"log"
 	"movie-api/auth"
+	"movie-api/config"
 	"movie-api/movie"
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "root:rootPassword!@tcp(127.0.0.1:3306)/movie-api-onboarding?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	log.Println("Successfully connected to database!")
+	db := config.ConnectToDb()
 
 	userRepository := auth.NewRepository(db)
 	userService := auth.NewService(userRepository)
