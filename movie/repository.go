557af84df2e260890d319c5982a8db44b6bb93a5
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-18 11:31:48 +0700

    develop repository for save new created movie with tags to db, and implement db transaction with gorm

--- movie/repository.go
+++ movie/repository.go
@@ -2,12 +2,13 @@ package movie
 
 import (
 	"errors"
-
+	movietag "movie-api/movie-tag"
 	"gorm.io/gorm"
 )
 
 type Repository interface {
 	GetMoviesWithTags() ([]Movie, error)
+	SaveNewMovieWithTags(movie Movie, movieTags []string) (Movie, []string, error)
 }
 
 type repository struct {
@@ -27,4 +28,42 @@ func (repository *repository) GetMoviesWithTags() ([]Movie, error) {
 	}
 
 	return movies, nil
+}
+
+func (repository *repository) SaveNewMovieWithTags(movie Movie, movieTags []string) (Movie, []string, error) {
+
+	transaction := repository.db.Begin()
+
+	err := repository.db.Create(&movie).Error
+	if err != nil {
+		return movie, movieTags, errors.New("failed to save new movie to database")
+	}
+ 
+
+	for _, newTag := range movieTags {
+		tag := movietag.Tag{
+			Name: newTag,
+		}
+
+		err = repository.db.Create(&tag).Error
+		if err != nil {
+			transaction.Rollback()
+			return movie, movieTags, errors.New("failed to save new tags to database")
+		}
+
+		movieTag := movietag.MovieTag{
+			MovieId: movie.ID,
+			TagId: tag.ID,
+		}
+
+		err = repository.db.Create(&movieTag).Error
+		if err != nil {
+			transaction.Rollback()
+			return movie, movieTags, errors.New("failed to save new tags to database")
+		}
+	}
+
+	transaction.Commit()
+
+	return movie, movieTags, nil
 }
