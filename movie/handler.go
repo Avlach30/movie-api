525fc25f89db5d3718e403c6513c9e7bc87db603
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-18 11:34:08 +0700

    develop handler for getting cleint request and give response to them when create new movie with tags

--- movie/handler.go
+++ movie/handler.go
@@ -3,7 +3,9 @@ package movie
 import (
 	"movie-api/helper"
 	"net/http"
+	"path/filepath"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type movieHandler struct {
@@ -24,4 +26,77 @@ func (handler *movieHandler) GetAllMovieWithTags(context *gin.Context) {
 
 	response := helper.ApiSuccessResponse("Get all movie with tags successfully", FormatResponseGetMovies(movies))
 	context.JSON(http.StatusOK, response)
+}
+
+func (handler *movieHandler) CreateNewMovieWithTags(context *gin.Context) {
+	input := CreateNewMovieInput{
+		Title: context.PostForm("title"),
+		Overview: context.PostForm("overview"),
+		PlayUntil: context.PostForm("play_until"),
+	}
+
+	err := context.ShouldBind(&input)
+	if err != nil {
+		errors := helper.ErrorValidationResponse(err)
+
+		errorMsg := gin.H{"errors": errors}
+
+		errorResponse := helper.ApiFailedResponse(errorMsg)
+		context.JSON(http.StatusUnprocessableEntity, errorResponse)
+		return
+	}
+
+
+	input.Tags = context.PostFormArray("tags")
+	
+
+	imagePoster, err := context.FormFile("poster")
+	if imagePoster == nil {
+		errorResponse := helper.ApiFailedResponse("Sorry, you must upload an poster image")
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+	if err != nil {
+		errorResponse := helper.ApiFailedResponse(err.Error())
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	extension := filepath.Ext(imagePoster.Filename)
+
+	if extension != ".jpg" && extension != ".jpeg" && extension != ".png" {
+		errorResponse := helper.ApiFailedResponse("Sorry, only image file type can be uploaded")
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	if imagePoster.Size > 1572864 {
+		errorResponse := helper.ApiFailedResponse("Sorry, image uploaded is more than limit (1,5 mb)")
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	newFileName := uuid.New().String() + extension
+
+	imagePath := "movie-poster/" + newFileName
+
+	err = context.SaveUploadedFile(imagePoster, imagePath)
+	if err != nil {
+		errorResponse := helper.ApiFailedResponse(err.Error())
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	input.Poster = imagePath
+
+
+	newMovie, err := handler.service.SaveNewMovieWithTags(input)
+	if (err != nil) {
+		errorResponse := helper.ApiFailedResponse(err.Error())
+		context.JSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	response := helper.ApiSuccessResponse("Create new movie with tags successfully", FormatResponseCreateNewMovie(newMovie))
+	context.JSON(http.StatusCreated, response)
 }
