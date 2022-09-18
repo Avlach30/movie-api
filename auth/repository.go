0985cd0087608637d5d302f41008c2d3f73f8405
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-18 12:08:04 +0700

    develop repository for find user by id from database with gorm

--- auth/repository.go
+++ auth/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Repository interface {
 	SignUp(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindUserById(id int) (User, error)
 }
 
 type repository struct {
@@ -29,6 +30,17 @@ func (repo *repository) FindByEmail(email string) (User, error) {
 	var user User
 
 	err := repo.db.Where("email = ?", email).Find(&user).Error
+	if (err != nil) {
+		return user, err
+	} else {
+		return user, nil
+	}
+}
+
+func (repo *repository) FindUserById(id int) (User, error) {
+	var user User
+
+	err := repo.db.Where("id = ?", id).Find(&user).Error
 	if (err != nil) {
 		return user, err
 	} else {
