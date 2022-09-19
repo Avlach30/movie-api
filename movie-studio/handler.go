a113cd736704daf6f70a8f9093c8b4cbe392f5be
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-19 14:06:36 +0700

    develop handler for get client request when create new movie studio and give response to them

---
+++ movie-studio/handler.go
@@ -0,0 +1,51 @@
+package moviestudio
+
+import (
+	"movie-api/auth"
+	"movie-api/helper"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type movieStudioHandler struct {
+	movieStudioService Service
+}
+
+func NewMovieStudioHandler(movieStudioService Service) *movieStudioHandler {
+	return &movieStudioHandler{movieStudioService}
+}
+
+func (handler *movieStudioHandler) CreateNewMovieStudio(context *gin.Context) {
+	loggedUser := context.MustGet("user").(auth.User)
+
+	if (!loggedUser.IsAdmin) {
+		errorResponse := helper.ApiFailedResponse("Sorry!, only admin can create new movie studio")
+		context.JSON(http.StatusForbidden, errorResponse)
+		return
+	}
+
+	var input CreateNewStudioInput
+
+	err := context.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.ErrorValidationResponse(err)
+
+		errorMsg := gin.H{"errors": errors}
+
+		errorResponse := helper.ApiFailedResponse(errorMsg)
+		context.JSON(http.StatusUnprocessableEntity, errorResponse)
+		return
+	}
+
+	movieStudio, err := handler.movieStudioService.SaveNewStudio(input)
+	if err != nil {
+		errorResponse := helper.ApiFailedResponse(err.Error())
+
+		context.JSON(http.StatusUnauthorized, errorResponse)
+		return
+	}
+	
+	successResponse := helper.ApiSuccessResponse("Create new movie studio successfully", movieStudio)
+
+	context.JSON(http.StatusCreated, successResponse)
+}
