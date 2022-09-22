64a8f3f45beb0cb174f991761b1815e91c475d9e
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-22 12:03:06 +0700

    calling necessary environtment variable for configure dsn connect gorm to database

--- config/database.go
+++ config/database.go
@@ -1,14 +1,24 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"movie-api/helper"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectToDb() (*gorm.DB) {
-	dsn := "root:rootPassword!@tcp(127.0.0.1:3306)/movie-api-onboarding?charset=utf8mb4&parseTime=True&loc=Local"
+
+	get := helper.GetEnvValue
+	DB_USER := get("DB_USER")
+	DB_PASSWORD := get("DB_PASSWORD")
+	DB_HOST := get("DB_HOST")
+	DB_PORT := get("DB_PORT")
+	DB_DATABASE := get("DB_DATABASE")
+	
+	dsn :=  fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
