a5b17b8f1ec6ff1aca6951899ae690ffab244b31
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-28 16:45:40 +0700

    update response configuration when falidation failed at movie module

--- movie/handler.go
+++ movie/handler.go
@@ -60,9 +60,7 @@ func (handler *movieHandler) CreateNewMovieWithTags(context *gin.Context) {
 	if err != nil {
 		errors := helper.ErrorValidationResponse(err)
 
-		errorMsg := gin.H{"errors": errors}
-
-		errorResponse := helper.ApiFailedResponse(errorMsg)
+		errorResponse := helper.ApiFailedResponse(errors)
 		context.JSON(http.StatusUnprocessableEntity, errorResponse)
 		return
 	}
