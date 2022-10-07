d442d09e891740bd678eb9127bb5b6702b890b75
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-10-07 21:53:00 +0700

    develop functior for structuring response of get playing now shcedule

--- movie-schedule/format.response.go
+++ movie-schedule/format.response.go
@@ -6,22 +6,32 @@ import (
 )
 
 type CreateNewScheduleResponse struct {
-	ID        int                `json:"id"`
+	ID        int                   `json:"id"`
 	Movie     movie.GetMovie        `json:"movie"`
 	Studio    moviestudio.GetStudio `json:"studio"`
-	StartTime string             `json:"start_time"`
-	EndTime   string             `json:"end_time"`
-	Price     int                `json:"price"`
-	Date      string             `json:"date"`
+	StartTime string                `json:"start_time"`
+	EndTime   string                `json:"end_time"`
+	Price     int                   `json:"price"`
+	Date      string                `json:"date"`
+}
+
+type GetPlayingNowScheduleResponse struct {
+	ID        int                   `json:"id"`
+	Movie     movie.MovieResponse   `json:"movie"`
+	Studio    moviestudio.GetStudio `json:"studio"`
+	StartTime string                `json:"start_time"`
+	EndTime   string                `json:"end_time"`
+	Price     int                   `json:"price"`
+	Date      string                `json:"date"`
 }
 
 func FormatCreateNewScheduleResponse(schedule MovieSchedule, movie movie.Movie, studio moviestudio.Studio) CreateNewScheduleResponse {
 	format := CreateNewScheduleResponse{
-		ID: schedule.ID,
+		ID:        schedule.ID,
 		StartTime: schedule.StartTime,
-		EndTime: schedule.EndTime,
-		Price: schedule.Price,
-		Date: schedule.Date,
+		EndTime:   schedule.EndTime,
+		Price:     schedule.Price,
+		Date:      schedule.Date,
 	}
 
 	format.Movie.Title = movie.Title
@@ -30,6 +40,40 @@ func FormatCreateNewScheduleResponse(schedule MovieSchedule, movie movie.Movie, 
 	format.Studio.ID = studio.ID
 	format.Studio.StudioNumber = studio.StudioNumber
 	format.Studio.SeatCapacity = studio.SeatCapacity
+
+	return format
+}
+
+func FormatGetEachPlayingNowScheduleResponse(schedule MovieSchedule, movie movie.Movie, studio moviestudio.Studio) GetPlayingNowScheduleResponse {
+	format := GetPlayingNowScheduleResponse{
+		ID: schedule.ID,
+		StartTime: schedule.StartTime,
+		EndTime: schedule.EndTime,
+		Price: schedule.Price,
+		Date: schedule.Date,
+	}
+
+	format.Movie.ID = movie.ID
+	format.Movie.Title = movie.Title
+	format.Movie.Overview = movie.Overview
+	format.Movie.Poster = movie.Poster
+	format.Movie.PlayUntil = movie.PlayUntil
+
+	format.Studio.ID = studio.ID
+	format.Studio.StudioNumber = studio.StudioNumber
+	format.Studio.SeatCapacity = studio.SeatCapacity
+
+	return format
+}
+
+func FormatGetPlayingNowSchedulesResponse(schedules []MovieSchedule) []GetPlayingNowScheduleResponse {
+	format := []GetPlayingNowScheduleResponse{}
+
+	for _, schedule := range schedules {
+		eachScheduleFormatter := FormatGetEachPlayingNowScheduleResponse(schedule, schedule.Movie, schedule.Studio)
+
+		format = append(format, eachScheduleFormatter)
+	}
 
 	return format
 }
